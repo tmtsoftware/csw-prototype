b992acfceb070b955ea4891860dffb4daca36190
Allan Brighton <Allan.Brighton@t-online.de>
2014-03-18 20:01:35 +0100

    Merged some small test classes

--- test/pkg/container1/src/main/scala/org/tmt/csw/test/container1/Container1.scala
+++ test/pkg/container1/src/main/scala/org/tmt/csw/test/container1/Container1.scala
@@ -2,11 +2,12 @@ package org.tmt.csw.test.container1
 
 import akka.actor._
 import akka.kernel.Bootable
-
-/**
- * A test that runs each of the classes below in a separate JVM (See the sbt-multi-jvm plugin)
- */
-
+import akka.util.Timeout
+import org.tmt.csw.pkg.Container
+import org.tmt.csw.cmd.akka.CommandStatus
+import org.tmt.csw.cmd.spray.CommandServiceHttpServer
+import scala.concurrent.duration._
+import akka.pattern.ask
 
 // This class is started by the Akka microkernel in standalone mode
 class Container1 extends Bootable {
@@ -21,5 +22,51 @@ class Container1 extends Bootable {
     system.shutdown()
   }
 }
+
+// The main actor for this application
+class Container1Actor extends Actor with ActorLogging {
+  import scala.concurrent.duration._
+  val duration: FiniteDuration = 5.seconds
+  implicit val timeout = Timeout(duration)
+  implicit val dispatcher = context.system.dispatcher
+
+  val container = Container.create("Container-1")
+  val assembly1Props = Assembly1.props("Assembly-1")
+
+  // Receive actor messages
+  def receive: Receive = {
+    case status: CommandStatus => log.info(s"received command status: $status")
+    case x => log.warning(s"received unknown message $x")
+  }
+
+  for {
+    assembly1 <- (container ? Container.CreateComponent(assembly1Props, "Assembly-1")).mapTo[ActorRef]
+  } {
+    // Start a HTTP server with the REST interface
+    val interface = Container1Settings(context.system).interface
+    val port = Container1Settings(context.system).port
+    val timeout = Container1Settings(context.system).timeout
+    context.actorOf(CommandServiceHttpServer.props(assembly1, interface, port, timeout), "commandService")
+  }
+}
+
+
+/**
+ * The settings for Container1
+ */
+object Container1Settings extends ExtensionKey[Container1Settings]
+
+class Container1Settings(system: ExtendedActorSystem) extends Extension {
+
+  // The network interface the service gets bound to, e.g. `"localhost"`.
+  val interface: String = system.settings.config getString "csw.test.assembly1-http.interface"
+
+  // The port the service gets bound to, e.g. `8080`.
+  val port: Int = system.settings.config getInt "csw.test.assembly1-http.port"
+
+  // The amount of time to wait when polling for the command status
+  val timeout: FiniteDuration = Duration(system.settings.config getMilliseconds "csw.test.assembly1-http.timeout", MILLISECONDS)
+}
+
 
 
--- test/pkg/container2/src/main/scala/org/tmt/csw/test/container2/Container2.scala
+++ test/pkg/container2/src/main/scala/org/tmt/csw/test/container2/Container2.scala
@@ -1,7 +1,10 @@
 package org.tmt.csw.test.container2
 
-import akka.actor.{Props, ActorSystem}
+import akka.actor._
 import akka.kernel.Bootable
+import akka.util.Timeout
+import org.tmt.csw.pkg.Container
+import akka.pattern.ask
 
 
 // This class is started by the Akka microkernel in standalone mode
@@ -19,3 +22,37 @@ class Container2 extends Bootable {
 }
 
 
+/**
+ * The main actor for this application.
+ * This container holds the two HCDs.
+ */
+class Container2Actor extends Actor with ActorLogging {
+  import scala.concurrent.duration._
+  val duration: FiniteDuration = 2.seconds
+  implicit val timeout = Timeout(duration)
+  implicit val dispatcher = context.system.dispatcher
+
+  def receive: Receive = {
+    case x => log.info(s"Received unknown message: $x")
+  }
+  val container = Container.create("Container-2")
+
+  val hcd2aProps = Hcd2.props("HCD-2A", "config.tmt.mobie.blue.filter")
+  val hcd2bProps = Hcd2.props("HCD-2B", "config.tmt.mobie.blue.disperser")
+
+  // For the Play Framework Demo
+  val hcd2cProps = Hcd2.props("HCD-2C", "config.tmt.tel.base.pos")
+  val hcd2dProps = Hcd2.props("HCD-2D", "config.tmt.tel.ao.pos.one")
+
+  for {
+    hcd2a <- (container ? Container.CreateComponent(hcd2aProps, "HCD-2A")).mapTo[ActorRef]
+    hcd2b <- (container ? Container.CreateComponent(hcd2bProps, "HCD-2B")).mapTo[ActorRef]
+    hcd2c <- (container ? Container.CreateComponent(hcd2cProps, "HCD-2C")).mapTo[ActorRef]
+    hcd2d <- (container ? Container.CreateComponent(hcd2dProps, "HCD-2D")).mapTo[ActorRef]
+  } {
+    log.info(s"Created HCD-2A: $hcd2a")
+    log.info(s"Created HCD-2B: $hcd2b")
+    log.info(s"Created HCD-2C: $hcd2c")
+    log.info(s"Created HCD-2D: $hcd2d")
+  }
+}
