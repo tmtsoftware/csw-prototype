4d0f428f8ee4b3719521eaf46795f2701479873c
Allan Brighton <Allan.Brighton@t-online.de>
2014-05-21 00:21:06 +0200

    Split projects to new repos

--- extjs/assembly1/app/model/Disperser.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Assembly1.model.Disperser', {
-    extend: 'Ext.data.Model',
-
-    fields: [
-        { name: 'disperser', mapping: 'tmt.mobie.blue.disperser.value' }
-    ]
-});
--- extjs/assembly1/app/model/Filter.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Assembly1.model.Filter', {
-    extend: 'Ext.data.Model',
-
-    fields: [
-        { name: 'filter', mapping: 'tmt.mobie.blue.disperser.value' }
-    ]
-});
