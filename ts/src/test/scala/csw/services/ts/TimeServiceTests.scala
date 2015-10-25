818933f1cce4cdf120b55245232d2ab89d6e1a96
K Gillies <kgillies@tmt.org>
2015-10-24 22:41:39 -0400

    Added Prototype TimeService
    Added very simple and possibly adequate Time Service

---
+++ ts/src/test/scala/csw/services/ts/TimeServiceTests.scala
@@ -0,0 +1,102 @@
+package csw.services.ts
+
+import java.time.Duration
+import java.time._
+
+import akka.actor._
+import akka.testkit.{TestKit, ImplicitSender}
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.scalatest.{BeforeAndAfterAll, FunSuiteLike}
+
+
+/**
+  * Created by gillies on 10/23/15.
+  */
+class TimeServiceTests extends TestKit(ActorSystem("Test")) with ImplicitSender with FunSuiteLike
+  with LazyLogging with BeforeAndAfterAll {
+
+  test("Basic Java Time Tests") {
+    import TimeService._
+
+    // Assume an eastern time zone for tests
+    val eclock = Clock.system(ZoneId.of("America/New_York"))
+    val hoursFromNyToHI = 6
+
+    // Can't do much test to see now equal implying bad clocks
+    val nyNow = LocalTime.now(eclock)
+    val hwNow = hawaiiLocalTimeNow
+    val now = localTimeNow
+
+    assert(nyNow.isAfter(hwNow))
+    // Try to determine that we have Hawaii time
+    assert(hwNow.equals(now) == false)  // Not the same, good
+    val diffHwNy = Duration.between(nyNow, hwNow) // Difference should be 6 hrs
+    assert(Math.abs(diffHwNy.toHours) == hoursFromNyToHI)
+
+    val utcNow = UTCTimeNow
+    val gpsNow:LocalTime = GPSTimeNow
+    val taiNow = TAITimeNow
+
+    assert(gpsNow.isAfter(utcNow))
+    assert(taiNow.isAfter((utcNow)))
+    assert(taiNow.isAfter((gpsNow)))
+
+  }
+
+  test("Call scheduler once") {
+    import scala.concurrent.duration._
+
+    val timerTest = system.actorOf(Props(new TestScheduler("tester", self)))
+    timerTest ! "once"
+
+    within((10.seconds)) {
+      expectMsg("done")
+    }
+
+  }
+
+  test("Call scheduler (with 5 counts in 5 seconds)") {
+    import scala.concurrent.duration._
+
+    val timerTest = system.actorOf(Props(new TestScheduler("tester", self)))
+    timerTest ! "five"
+
+    within((10.seconds)) {
+      val cancellable = expectMsgType[Cancellable]
+      logger.info(s"Received cancellable: $cancellable")
+      val count = expectMsgType[Int]
+      logger.info(s"Executed $count scheduled messages")
+      assert(count == 5)
+      cancellable.cancel()
+    }
+
+  }
+
+  case class TestScheduler(name: String, caller: ActorRef) extends Actor with ActorLogging with TimeService.TimeServiceScheduler {
+    import TimeService._
+
+    var count = 0;
+
+    def receive: Receive = {
+      case "once" => {
+        log.info("Received once start")
+        val c = scheduleOnce(localTimeNow.plusSeconds(5), context.self, "once-done")
+      }
+      case "five" => {
+        log.info("Received multi start")
+        val c = schedule(localTimeNow.plusSeconds(1), java.time.Duration.ofSeconds(1), context.self, "count")
+        caller ! c  //Return the cancellable
+      }
+      case "count" => {
+        count = count + 1
+        log.info(s"Count: $count")
+        if (count >= 5) caller ! count
+      }
+      case "once-done" => {
+        log.info("Received Done")
+        caller ! "done"
+      }
+    }
+  }
+
+}
