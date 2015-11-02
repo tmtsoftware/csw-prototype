e7468f102641a0733082f05742e8ab36a45552b2
Allan Brighton <Allan.Brighton@t-online.de>
2015-11-02 23:53:41 +0100

    Updating scaladocs

---
+++ ts/src/main/scala/csw/services/ts/package.scala
@@ -0,0 +1,12 @@
+package csw.services
+
+/**
+  * The TimeService provides a component actor with timed messages:
+  *
+  * - scheduleOne -  sends a message to an actor once some time in the future
+  *
+  * - schedule    -  waits until a specific time and then sends periodic message to an actor until cancelled
+  */
+package object ts {
+
+}
