b0fc14c76b015fd7da5dfa241fc46783280bf628
Allan Brighton <Allan.Brighton@t-online.de>
2016-03-01 20:15:37 +0100

    start svn test

---
+++ cs/src/test/scala/csw/services/cs/core/svn/SvnConfigManagerTests.scala
@@ -0,0 +1,22 @@
+package csw.services.cs.core.svn
+
+import akka.actor.ActorSystem
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import csw.services.apps.configServiceAnnex.ConfigServiceAnnexServer
+import org.scalatest.FunSuite
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+/**
+  * Created by abrighto on 29/02/2016.
+  */
+class SvnConfigManagerTests extends FunSuite with LazyLogging {
+//  implicit val system = ActorSystem()
+
+  test("Test creating a SvnConfigManager, storing and retrieving some files") {
+
+  }
+
+}
+
