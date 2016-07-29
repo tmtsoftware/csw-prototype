3baa142126da06d386022354fdf81da2008c24fe
abrighton <allanexus@gmail.com>
2016-07-29 10:58:12 +0200

    Removed boot2docker specific code

--- cs/docker-build.sh
+++ cs/docker-build.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
 #
 # Builds a docker image for the config service (cs).
-#
-# Note: This script tries to run in the docker environment, using the boot2docker shell, if found (on Mac OS).
-# To start again from scratch, run these commands in a bash shell (assumes boot2docker is installed):
-#
-# boot2docker delete
-# boot2docker init
-# boot2docker up
 
 # user name for docker (Change this to your docker user name if you want to push to docker hub)
 user=$USER
 
 # version tag
 version=latest
-
-# Get the IP address (Use boot2docker for Mac. Linux it should be the same IP?)
-if  [ `which boot2docker` ] ; then
-    eval "$(boot2docker shellinit)"
-fi
 
 cd ..
 sbt "project cs" docker:stage || exit 1
--- cs/docker-run.sh
+++ cs/docker-run.sh
@@ -5,11 +5,5 @@
 # user name for docker (Change this to your docker user name if you want to push to docker hub)
 user=$USER
 
-# Get the IP address (Use boot2docker for Mac. Linux it should be the same IP?)
-if  [ `which boot2docker` ] ; then
-    eval "$(boot2docker shellinit)"
-fi
-
-# Start the application
+# Start the application (--net host option is needed when working with mDNS based location service)
 docker run --net host -t -P --name cs $user/cs || exit 1
-
