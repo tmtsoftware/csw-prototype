7da2257c08a55d34c18010faef7288cfcce6b476
abrighton <allanexus@gmail.com>
2016-07-29 20:30:15 +0200

    Testing Mac version

--- cs/docker-run.sh
+++ cs/docker-run.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
 # Starts the docker image for the config service (cs)
-
-# Port to use for Config Service
-port=9999
+#
+# Note: If you want to run this script on a Mac: The Config Service uses the Location Service,
+# which is based on Multicast DNS to advertise it's location. That doesn't work well with VirtualBox on
+# the Mac, which docker uses. Not sure what the right network settings would be for that.
 
 # user name for docker (Change this to your docker user name if you want to push to docker hub)
 user=$USER
 
 # Start the application
-docker run --net host -t -P -p $port:$port --name cs $user/cs \
-    -Dakka.remote.netty.tcp.port=$port  \
-    || exit 1
+docker run --net host -t -P --name cs $user/cs || exit 1
 
-# Note: For Mac users: Use Bridged Networking to get avahi/mDns to work for Location Service
-# (select in VirtualBox manager under Network).
-# If using VirtualBox/boot2docker, may need to run this once the application is running to expose ports
-# VBoxManage controlvm "boot2docker-vm" natpf1 "tcp-port9999,tcp,,9999,,9999";
