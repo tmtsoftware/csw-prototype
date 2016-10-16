d9f71fbff5b0e294042037f79181f688335395ca
abrighton <allanexus@gmail.com>
2016-10-16 23:10:29 +0200

    Started porting Scala to Java, minor fixes for Scala source (Things I noticed while porting)

--- examples/vsliceJava/src/main/java/csw/examples/vsliceJava/assembly/Algorithms.java
+++ examples/vsliceJava/src/main/java/csw/examples/vsliceJava/assembly/Algorithms.java
@@ -4,14 +4,16 @@ import akka.japi.Pair;
 import csw.util.config.DoubleItem;
 import csw.examples.vsliceJava.assembly.AssemblyContext.*;
 
+import static javacsw.util.config.JItems.jvalue;
+
 /**
  * This object contains functions that implement this test version of the trombone assembly algorithms.
- *
+ * <p>
  * These algorithms are representative in spirit, but not technically correct. The actual code will be
  * a closed loop and continually recalculate the elevation. Since the correctness of the algorithms was
  * not really the focus of the vertical slice, I wanted to just get something that sort of did the
  * right things and had the same variables.
- *
+ * <p>
  * The algorithms here are implemented in an object so they can be tested independently.  I think this is
  * a good strategy.  Then they are called from the FollowActor and other places. The method names are
  * representative of their function: rangeDistanceToElevation for instance.
@@ -25,7 +27,9 @@ public class Algorithms {
    * @param zenithAngle DoubleItem that contains zenith angle
    * @return true if valid else false
    */
-  public static boolean verifyZenithAngle(DoubleItem zenithAngle) { return zenithAngle.head() < 90.0 && zenithAngle.head() >= 0.0; }
+  public static boolean verifyZenithAngle(DoubleItem zenithAngle) {
+    return jvalue(zenithAngle) < 90.0 && jvalue(zenithAngle) >= 0.0;
+  }
 
   /**
    * Checking the input focus error against fake limits of +/- 20
@@ -33,13 +37,17 @@ public class Algorithms {
    * @param focusError DoubleItem that contains focusError
    * @return true if valid else false
    */
-  public  static boolean verifyFocusError(TromboneCalculationConfig calculationConfig, DoubleItem focusError) {
-    return focusError.head() >= calculationConfig.lowerFocusLimit && focusError.head() <= calculationConfig.upperFocusLimit;
+  public static boolean verifyFocusError(TromboneCalculationConfig calculationConfig, DoubleItem focusError) {
+    return jvalue(focusError) >= calculationConfig.lowerFocusLimit && jvalue(focusError) <= calculationConfig.upperFocusLimit;
   }
 
-  public static double zenithAngleToRangeDistance(double elevation, double zenithAngle) { return elevation / Math.cos(Math.toRadians(zenithAngle));}
+  public static double zenithAngleToRangeDistance(double elevation, double zenithAngle) {
+    return elevation / Math.cos(Math.toRadians(zenithAngle));
+  }
 
-  public static double rangeDistanceToElevation(double rangeDistance, double zenithAngle) { return Math.cos(Math.toRadians(zenithAngle)) * rangeDistance;}
+  public static double rangeDistanceToElevation(double rangeDistance, double zenithAngle) {
+    return Math.cos(Math.toRadians(zenithAngle)) * rangeDistance;
+  }
 
   public static Pair<Double, Double> focusZenithAngleToElevationAndRangeDistance(TromboneCalculationConfig calculationConfig,
                                                                                  double elevation, double focusError, double zenithAngle) {
@@ -69,7 +77,9 @@ public class Algorithms {
    * @param rangeDistance in kilometer units
    * @return stage position in millimeters
    */
-  public static double rangeDistanceToStagePosition(double rangeDistance) { return rangeDistance; }
+  public static double rangeDistanceToStagePosition(double rangeDistance) {
+    return rangeDistance;
+  }
 
   /**
    * @param stagePosition is the value of the stage position in millimeters (currently the total NA elevation)
@@ -77,7 +87,7 @@ public class Algorithms {
    */
   public static int stagePositionToEncoder(TromboneControlConfig controlConfig, double stagePosition) {
     // Scale value to be between 200 and 1000 encoder
-    int encoderValue = (int)(controlConfig.positionScale * (stagePosition - controlConfig.stageZero) + controlConfig.minStageEncoder);
+    int encoderValue = (int) (controlConfig.positionScale * (stagePosition - controlConfig.stageZero) + controlConfig.minStageEncoder);
     return Math.max(controlConfig.minEncoderLimit, Math.min(controlConfig.maxEncoderLimit, encoderValue));
   }
 
