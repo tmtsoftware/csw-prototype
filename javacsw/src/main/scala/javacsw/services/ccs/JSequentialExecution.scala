eb0028bbbc5772bbdd3f8a1cfc30811ce8c92a33
abrighton <allanexus@gmail.com>
2016-10-24 10:50:23 +0200

    In progress port of vslice example to Java

--- javacsw/src/main/scala/javacsw/services/ccs/JSequentialExecution.scala
+++ javacsw/src/main/scala/javacsw/services/ccs/JSequentialExecution.scala
@@ -10,8 +10,8 @@ import csw.util.config.Configurations.SetupConfigArg
 import scala.compat.java8.OptionConverters._
 
 /**
-  * Enable Java access to SequentialExecution objects
-  */
+ * Enable Java access to SequentialExecution objects
+ */
 object JSequentialExecution {
 
   def props(sca: SetupConfigArg, commandOriginator: Optional[ActorRef]): Props = SequentialExecutor.props(sca, commandOriginator.asScala)
