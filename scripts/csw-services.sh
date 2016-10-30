2f457e9875bdac2b9d40a65122aa260a789fb3fd
abrighton <allanexus@gmail.com>
2016-10-30 15:19:52 +0100

    Updated csw-services.sh script

--- scripts/csw-services.sh
+++ scripts/csw-services.sh
@@ -32,8 +32,8 @@ fi
 # Set to yes to start the config service
 START_CONFIG_SERVICE=no
 
-REDIS1_PORT=7777
-#REDIS2_PORT=...
+REDIS_PORT=7777
+REDIS_SERVICES="Event Service,Alarm Service,Telemetry Service"
 
 OS=`uname`
 
@@ -44,14 +44,12 @@ test -d $CSW_DATA_DIR || mkdir -p $CSW_DATA_DIR
 # Config Service pid and log files
 CS_PID_FILE=$CSW_DATA_DIR/cs.pid
 CS_LOG_FILE=$CSW_DATA_DIR/cs.log
+# Config Service options
+CS_OPTIONS="--init --nohttp --noannex"
 
 # Redis pid and log files
-REDIS1_PID_FILE=$CSW_DATA_DIR/redis1.pid
-REDIS1_LOG_FILE=$CSW_DATA_DIR/redis1.log
-
-# Log and pid file for additional pids for avahi or dns-sd processes
-REG_PID_FILE=$CSW_DATA_DIR/reg.pids
-REG_LOG_FILE=$CSW_DATA_DIR/reg.log
+REDIS_PID_FILE=$CSW_DATA_DIR/redis1.pid
+REDIS_LOG_FILE=$CSW_DATA_DIR/redis1.log
 
 case "$1" in
     start)
@@ -60,72 +58,54 @@ case "$1" in
         if [ ! -d "$CSW_INSTALL" ] ; then
             echo "Please set CSW_INSTALL to the root directory where the csw software is installed"
             exit 1
-        elif [ "$START_CONFIG_SERVICE" == "yes" ] ; then
-            $CSW_INSTALL/bin/cs --init --nohttp --noannex > $CS_LOG_FILE 2>&1 &
-            echo $! > $CS_PID_FILE
-        fi
-
-	# Start Redis and register Redis based services
-        if [ -f $REDIS1_PID_FILE ] ; then
-            echo "Redis $REDIS1_PID_FILE exists, process is already running or crashed"
         else
-            if [ $OS != "Linux" -a "$OS" != "Darwin" ] ; then
-                echo "This script only supports Linux and Mac OS"
-                exit 1
-            fi
-            echo "Starting Redis server on port $REDIS1_PORT..."
-            rm -f $REG_PID_FILE $REG_LOG_FILE $REDIS1_LOG_FILE
-            $REDIS_SERVER --port $REDIS1_PORT --protected-mode no --daemonize yes --pidfile $REDIS1_PID_FILE --logfile $REDIS1_LOG_FILE
-            if [ "$OS" == "Linux" ] ; then
-                avahi-publish -s "Event Service-Service-tcp" _csw._tcp $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
-                avahi-publish -s "Alarm Service-Service-tcp" _csw._tcp $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
-                avahi-publish -s "Telemetry Service-Service-tcp" _csw._tcp $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
-            else
-                dns-sd -R "Event Service-Service-tcp" _csw._tcp local. $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
-                dns-sd -R "Alarm Service-Service-tcp" _csw._tcp local. $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
-                dns-sd -R "Telemetry Service-Service-tcp" _csw._tcp local. $REDIS1_PORT >> $REG_LOG_FILE 2>&1 &
-                echo $! >> $REG_PID_FILE
+            # Start Config Service
+            if [ "$START_CONFIG_SERVICE" == "yes" ] ; then
+                if [ -f $CS_PID_FILE ] ; then
+                    echo "Config Service pid file $CS_PID_FILE exists, process is already running or crashed?"
+                else
+                    $CSW_INSTALL/bin/cs $CS_OPTIONS > $CS_LOG_FILE 2>&1 &
+                    echo $! > $CS_PID_FILE
+                fi
             fi
 
-            # Load the default alarms in to the Alarm Service Redis instance
-            $CSW_INSTALL/bin/asconsole --init $CSW_INSTALL/conf/alarms.conf
+            # Start Redis based services using trackLocation
+            if [ -f $REDIS_PID_FILE ] ; then
+                echo "Redis pid file $REDIS_PID_FILE exists, process is already running or crashed?"
+            else
+                $CSW_INSTALL/bin/tracklocation --name "$REDIS_SERVICES" --command "$REDIS_SERVER --protected-mode no --port $REDIS_PORT" > $REDIS_LOG_FILE 2>&1 &
+                echo $! > $REDIS_PID_FILE
+				# Load the default alarms in to the Alarm Service Redis instance
+				$CSW_INSTALL/bin/asconsole --init $CSW_INSTALL/conf/alarms.conf
+			fi
         fi
         ;;
     stop)
         # Stop Redis
-        if [ ! -f $REDIS1_PID_FILE ]
+        if [ ! -f $REDIS_PID_FILE ]
         then
-            echo "Redis $REDIS1_PID_FILE does not exist, process is not running"
+            echo "Redis $REDIS_PID_FILE does not exist, process is not running"
         else
-            PID=$(cat $REDIS1_PID_FILE)
+            PID=$(cat $REDIS_PID_FILE)
             echo "Stopping Redis..."
-            $REDIS_CLIENT -p $REDIS1_PORT shutdown
+            $REDIS_CLIENT -p $REDIS_PORT shutdown
             while [ -x /proc/${PID} ]
             do
                 echo "Waiting for Redis to shutdown ..."
                 sleep 1
             done
             echo "Redis stopped"
-            # Stop the avahi/dns-sd processes
-            for pid in `cat $REG_PID_FILE`; do
-                kill $pid
-            done
-            rm -f $REG_PID_FILE $REG_LOG_FILE $REDIS1_LOG_FILE $REDIS1_PID_FILE
+            rm -f $REG_PID_FILE $REG_LOG_FILE $REDIS_LOG_FILE $REDIS_PID_FILE
         fi
         # Stop Config Service
         if [ "$START_CONFIG_SERVICE" == "yes" ] ; then
             if [ ! -f $CS_PID_FILE ]; then
-		        echo "Config Service $CS_PID_FILE does not exist, process is not running"
+				echo "Config Service $CS_PID_FILE does not exist, process is not running"
             else
-		        PID=$(cat $CS_PID_FILE)
-		        echo "Stopping Config Service..."
-		        kill $PID
-		        rm -f $CS_PID_FILE $CS_LOG_FILE
+				PID=$(cat $CS_PID_FILE)
+				echo "Stopping Config Service..."
+				kill $PID
+				rm -f $CS_PID_FILE $CS_LOG_FILE
             fi
         fi
         ;;
