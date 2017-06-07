3983625864bb0a98623a494affd56e8217b9704a
abrighton <78px3WT5s>
2017-06-07 19:09:51 +0200

    Renamed item sets to commands/parameter sets, updated variable names, parameters, comments

--- javacsw/src/main/java/javacsw/util/params/package-info.java
+++ javacsw/src/main/java/javacsw/util/params/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Provides easier Java access to some of the configuration related classes.
+ * Provides easier Java access to the parameter set based command and event classes.
  * See the {@link csw.util.param} package for more details.
  */
 package javacsw.util.params;
